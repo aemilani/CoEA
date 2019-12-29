3257891238a690c14eeda59931370970dc0b1572
aemilani <aealieftekhary@gmail.com>
2019-12-29 13:15:59 +0100

    Renamed create_coea.py to coea.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import time
 import datetime
-import create_coea
+import coea
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -13,16 +13,16 @@ net_weights = (1, -1)  # Rho_MK, ValLoss
 
 n_gens = 50  # Number of generations
 
-coea = create_coea.CoEA(pop_size_bits=6,
-                        n_layer_species=4,
-                        layer_weights=layer_weights,
-                        net_weights=net_weights,
-                        iters=1000,
-                        net_pop_size=80)
+ca = coea.CoEA(pop_size_bits=6,
+               n_layer_species=4,
+               layer_weights=layer_weights,
+               net_weights=net_weights,
+               iters=1000,
+               net_pop_size=80)
 
-toolbox = coea.toolbox
-net_population = coea.net_population
-layer_population = coea.layer_population
+toolbox = ca.toolbox
+net_population = ca.net_population
+layer_population = ca.layer_population
 
 
 print('\nEvaluating Initial Population...\n')
@@ -31,11 +31,11 @@ fits = toolbox.map(toolbox.evaluateNet, net_population)
 for fit, ind in zip(fits, net_population):
     ind.fitness.values = fit
 # order networks population
-net_population = coea.order_net_pop(net_population)
-coea.layers_credit_assignment(net_population)
+net_population = ca.order_net_pop(net_population)
+ca.layers_credit_assignment(net_population)
 # order layers population
 for i in range(len(layer_population)):
-    layer_population[i] = toolbox.selectNSGA2(layer_population[i], k=coea.layer_pop_size)
+    layer_population[i] = toolbox.selectNSGA2(layer_population[i], k=ca.layer_pop_size)
 print('\nInitial Population Fitness Values:\n')
 for ind in net_population:
     print(ind.fitness.values)
@@ -46,7 +46,7 @@ net_pops, layer_pops = [], []
 
 initial_net_pop = toolbox.clone(net_population)
 initial_layer_pop = toolbox.clone(layer_population)
-for i in range(coea.n_layer_species):
+for i in range(ca.n_layer_species):
     initial_layer_pop[i].sort(key=lambda x: x.index)
 net_pops.append(initial_net_pop)
 layer_pops.append(initial_layer_pop)
@@ -80,7 +80,7 @@ for gen in range(n_gens):
             toolbox.mutateNetStructure(ind)
             toolbox.mutateNetParameters(ind)
             del ind.fitness.values
-    n_net_top_inds = int(0.7 * coea.net_pop_size)  # number of top 70% network individuals
+    n_net_top_inds = int(0.7 * ca.net_pop_size)  # number of top 70% network individuals
     old_reached = False
     # used for calculating mutation probabilities
     fits = []
@@ -128,7 +128,7 @@ for gen in range(n_gens):
     for i in range(len(offspring)):
         net_population.append(offspring[i])
     # layer population evolution
-    n_layer_top_inds = int(0.7 * coea.layer_pop_size)  # number of top 70% layer individuals
+    n_layer_top_inds = int(0.7 * ca.layer_pop_size)  # number of top 70% layer individuals
     # delete worst 30% of layers
     deleted_indexes = []
     for i, species in enumerate(layer_population):
@@ -142,7 +142,7 @@ for gen in range(n_gens):
             remaining_indexes.append(ind.index)
         # random selection of layers to mutate
         new_idxs = list(np.random.choice(remaining_indexes,
-                                         size=coea.layer_pop_size - n_layer_top_inds,
+                                         size=ca.layer_pop_size - n_layer_top_inds,
                                          replace=False))
         assert len(del_indexes) == len(new_idxs)
         for j, idx in enumerate(new_idxs):
@@ -153,17 +153,17 @@ for gen in range(n_gens):
             species.append(child)
     # evaluation
     for ind in net_population:
-        for i in range(coea.n_layer_species):
+        for i in range(ca.n_layer_species):
             if ind.net_params['h{}'.format(i + 1)] in deleted_indexes[i]:
                 del ind.fitness.values
     invalid_net_inds = [ind for ind in net_population if not ind.fitness.valid]
     fits = map(toolbox.evaluateNet, invalid_net_inds)
     for ind, fit in zip(invalid_net_inds, fits):
         ind.fitness.values = fit
-    net_population = coea.order_net_pop(net_population)
-    coea.layers_credit_assignment(net_population)
+    net_population = ca.order_net_pop(net_population)
+    ca.layers_credit_assignment(net_population)
     for i in range(len(layer_population)):
-        layer_population[i] = toolbox.selectNSGA2(layer_population[i], k=coea.layer_pop_size)
+        layer_population[i] = toolbox.selectNSGA2(layer_population[i], k=ca.layer_pop_size)
     # saving fitnesses
     avg_species_fits = []
     min_species_fits = []
@@ -182,7 +182,7 @@ for gen in range(n_gens):
     max_net_rho.append(np.max(rhos))
     # saving populations
     pop = toolbox.clone(layer_population)
-    for i in range(coea.n_layer_species):
+    for i in range(ca.n_layer_species):
         pop[i].sort(key=lambda x: x.index)
     layer_pops.append(pop)
     pop = toolbox.clone(net_population)
@@ -218,7 +218,7 @@ plt.ylabel('Rho_MK')
 plt.title('Average Rho_MK per generation')
 plt.savefig(dirr + '/' + 'Average_Rho_MK_per_generation.png')
 
-for i in range(coea.n_layer_species):
+for i in range(ca.n_layer_species):
     plt.figure()
     plt.plot(np.array(avg_layer_fits)[:, i])
     plt.xlabel('Generation')
@@ -250,11 +250,11 @@ for pop in net_pops:
 # index of each chromosome of each generation population in the next generation population
 indexes = toolbox.clone(net_pop_ids)
 for i in range(len(net_pops) - 1):
-    for j in range(coea.net_pop_size):
+    for j in range(ca.net_pop_size):
         _ = np.equal(net_pop_ids[i][j], net_pop_ids[i + 1])
         if bool(np.isin(True, _)):
             indexes[i][j] = list(_).index(True)
         else:
             indexes[i][j] = None
-for k in range(coea.net_pop_size):
+for k in range(ca.net_pop_size):
     indexes[-1][k] = None
